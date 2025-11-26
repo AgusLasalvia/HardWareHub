bfefe36a55e192725e610e83b8ff5e94649da030
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-11-26 16:01:19 -0300

    add:layout + navbar

--- client/components/ui/Navbar/Navbar.tsx
+++ client/components/ui/Navbar/Navbar.tsx
@@ -1,20 +1,49 @@
-import Link from "next/link"
-import "./Navbar.css"
-import Image from "next/image"
+"use client";
+
+import Link from "next/link";
+import "./Navbar.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const links = [
   { label: "Home", href: "/home" },
   { label: "Discounts", href: "/discounts" },
   { label: "Categories", href: "/categories" },
-]
+];
+
 
 
 const Navbar = () => {
+
+  const [isLogged, setIsLogged] = useState(null)
+
   return (
     <nav>
-      <Link href="/home">
-        <Image src="/" alt="Company Logo" />
+      <Link href="/" className="nav-logo">
+        {/* <Image src="" width={20} height={20} alt="Company Logo" /> */}
       </Link>
+
+      <ul>
+        {links.map((link) => (
+          <li key={link.href} className="nav-link">
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+
+      {
+        isLogged ?
+          <div>
+            <label>Profle</label>
+          </div>
+          :
+          <Link href="/login">
+            Login / Sing in
+          </Link>
+      }
+
     </nav>
-  )
-}
+  );
+};
+
+export default Navbar;
